8e5f06ec4c975db5743a7236237fed794d8be222
Sam Liang <darkdragn20000@gmail.com>
2021-03-06 22:00:56 -0500

    UI code

--- reg.py
+++ reg.py
@@ -8,7 +8,7 @@ from socket import socket, SOL_SOCKET, SO_REUSEADDR
 from pickle import dump, load
 from sqlite3 import connect
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QFrame
-from PyQt5.QtWidgets import QLabel, QGridLayout, QPushButton
+from PyQt5.QtWidgets import QLabel, QGridLayout, QPushButton, QVBoxLayout, QFormLayout, QHBoxLayout
 from PyQt5.QtWidgets import QLineEdit, QTextEdit, QScrollArea
 from PyQt5.QtWidgets import QSlider, QCheckBox, QRadioButton
 from PyQt5.QtWidgets import QListWidget, QDesktopWidget
@@ -16,21 +16,51 @@ from PyQt5.QtCore import Qt
 import argparse
 import textwrap
 from database_handler import create_sql_command
-from regdetails import course_details
 
 # regserver.py handles database
+
+
+def mousePressEvent(self, QMouseEvent):
+    if app.mouseButtons() & QtCore.Qt.LeftButton:
+        self.clicked.emit()
+
+
+def mouseReleaseEvent(self, QMouseEvent):
+    cursor = QtGui.QCursor()
+    print(cursor.pos())
+    submit_but.clicked.connect(buttonSlot)
+
+
+def buttonSlot():
+    QMessageBox.information(window, 'Class Details')
+    textEdit.append()
+
+# source: https://www.programcreek.com/python/example/101657/PyQt5.QtCore.Qt.Key_Up
+
+
+def keyPressEvent(self, event):
+    key = event.key()
+    # if key == Qt.Key_Tab:
+
+    # https://stackoverflow.com/questions/43304803/moving-the-cursor-in-a-pyqt5-text-edit-doesnt-work
+    # cursor.movePosition(), cursor.movePosition(cursor.Left, cursor.KeepAnchor,  3)
+    # elif key == Qt.Key_Up:
+
+    # elif key == Qt.Key_Left:
+
+    # elif key == Qt.Key_Enter:
 
 
 def main(argv):
     # argparse is user-interface related code
     # Create parser that has a description of the program and host/port positional arguments
-    parser = argparse.ArgumentParser(
-        description='Client for the registrar application', allow_abbrev=False)
-    parser.add_argument(
-        "host", type=str, help="the host on which the server is running", nargs=1)
-    parser.add_argument(
-        "port", type=int, help="the port at which the server is listening", nargs=1)
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description='Client for the registrar application', allow_abbrev=False)
+    # parser.add_argument(
+    #     "host", type=str, help="the host on which the server is running", nargs=1)
+    # parser.add_argument(
+    #     "port", type=int, help="the port at which the server is listening", nargs=1)
+    # args = parser.parse_args()
 
     # database code
     try:
@@ -38,40 +68,147 @@ def main(argv):
         app = QApplication(argv)
 
         # Textfields for a dept, coursenum, area, title
-        deptLab = QLabel("Dept:")
+        deptLab = QLabel("Dept: ")
+        deptLab.setAlignment(Qt.AlignRight)
         deptLine = QLineEdit()
-        courseLab = QLabel("Number:")
+        courseLab = QLabel("Number: ")
+        courseLab.setAlignment(Qt.AlignRight)
         courseNumLine = QLineEdit()
-        areaLab = QLabel("Area:")
+        areaLab = QLabel("Area: ")
+        areaLab.setAlignment(Qt.AlignRight)
         areaLine = QLineEdit()
-        titleLab = QLabel("Title:")
+        titleLab = QLabel("Title: ")
+        titleLab.setAlignment(Qt.AlignRight)
         titleLine = QLineEdit()
         listWidget = QListWidget()
+
+        # list box that can scroll vertically and horizontally
+        list_box = QListWidget()
 
         # submit button
         submit_but = QPushButton("Submit")
 
-        # FIX THIS
-        def buttonSlot():
-            QMessageBox.information(window, 'Class Details')
-            textEdit.append()
+        top_layout = QGridLayout()
+        top_layout.setSpacing(0)
+        top_layout.setContentsMargins(0, 0, 0, 0)
+        top_layout.setRowStretch(0, 0)
+        top_layout.setRowStretch(1, 0)
+        top_layout.setRowStretch(2, 0)
+        top_layout.setRowStretch(3, 0)
+        top_layout.setColumnStretch(0, 0)
+        top_layout.setColumnStretch(1, 1)
+        top_layout.setColumnStretch(2, 0)
 
-        # source: https://www.programcreek.com/python/example/101657/PyQt5.QtCore.Qt.Key_Up
-        def keyPressEvent(self, event):
-            key = event.key()
-            if (key == Qt.Key_Tab):
+        # labels
+        top_layout.addWidget(deptLab, 0, 0)
+        top_layout.addWidget(courseLab, 1, 0)
+        top_layout.addWidget(areaLab, 2, 0)
+        top_layout.addWidget(titleLab, 3, 0)
 
-            elif (key == Qt.Key_Up):
+        # line edits
+        top_layout.addWidget(deptLine, 0, 1)
+        top_layout.addWidget(courseNumLine, 1, 1)
+        top_layout.addWidget(areaLine, 2, 1)
+        top_layout.addWidget(titleLine, 3, 1)
 
-            elif (key == Qt.Key_Left):
+        # submit button
+        top_layout.addWidget(submit_but, 0, 2, alignment=Qt.AlignCenter)
 
-            elif (key == Enter):
+        # top layout frame
+        top_frame = QFrame()
+        top_frame.setLayout(top_layout)
 
-        def
-        submit_but.clicked.connect(buttonSlot)
+        # bottom list layout frame
+        bot_layout = QGridLayout()
+        bot_layout.setSpacing(0)
+        bot_layout.setContentsMargins(0, 0, 0, 0)
+        bot_layout.addWidget(list_box, 0, 0)
+        bot_frame = QFrame()
+        bot_frame.setLayout(bot_layout)
 
-        # list box that can scroll vertically and horizontally
-        list_box = QListWidget()
+        # overarching frame layout and frame
+        centralFrameLayout = QGridLayout()
+        centralFrameLayout.setSpacing(0)
+        centralFrameLayout.setContentsMargins(0, 0, 0, 0)
+        centralFrameLayout.setRowStretch(0, 0)
+        centralFrameLayout.setRowStretch(1, 1)
+        centralFrameLayout.setColumnStretch(0, 1)
+        centralFrameLayout.addWidget(top_frame, 0, 0)
+        centralFrameLayout.addWidget(bot_frame, 1, 0)
+        centralFrame = QFrame()
+        centralFrame.setLayout(centralFrameLayout)
+
+        # ---------------------------------------------------
+
+        # outer_layout = QVBoxLayout()
+        # outer_layout.setSpacing(0)
+        # outer_layout.setContentsMargins(0, 0, 0, 0)
+
+        # top_layout = QHBoxLayout()
+        # top_layout.setSpacing(0)
+        # top_layout.setContentsMargins(0, 0, 0, 0)
+        # top_layout.setStretch(0, 0)
+        # top_layout.setStretch(1, 1)
+        # top_layout.setStretch(2, 0)
+
+        # top_layout_left = QVBoxLayout()
+        # top_layout_left.setAlignment(Qt.AlignRight)
+        # top_layout_left.addWidget(deptLab)
+        # courseLab.setAlignment(Qt.AlignRight)
+        # top_layout_left.addWidget(courseLab)
+        # areaLab.setAlignment(Qt.AlignRight)
+        # top_layout_left.addWidget(areaLab)
+        # titleLab.setAlignment(Qt.AlignRight)
+        # top_layout_left.addWidget(titleLab)
+        # # top_layout_left.addRow("Dept: ", deptLine)
+        # # top_layout_left.addRow("Number: ", courseNumLine)
+        # # top_layout_left.addRow("Area: ", areaLine)
+        # # top_layout_left.addRow("Title: ", titleLine)
+
+        # top_layout_middle = QVBoxLayout()
+        # top_layout_middle.addWidget(deptLine)
+        # top_layout_middle.addWidget(courseNumLine)
+        # top_layout_middle.addWidget(areaLine)
+        # top_layout_middle.addWidget(titleLine)
+
+        # top_layout_right = QVBoxLayout()
+        # top_layout_right.setAlignment(Qt.AlignRight)
+        # top_layout_right.addWidget(submit_but)
+
+        # top_layout.addLayout(top_layout_left)
+        # top_layout.addLayout(top_layout_middle)
+        # top_layout.addLayout(top_layout_right)
+
+        # list_layout = QVBoxLayout()
+        # list_layout.addWidget(list_box)
+
+        # outer_layout.addLayout(top_layout)
+        # outer_layout.addLayout(list_layout)
+        # # layout.addWidget(deptLab)
+        # # layout.addWidget(courseLab)
+        # # layout.addWidget(areaLab)
+        # # layout.addWidget(titleLab)
+        # # layout.addWidget(deptLine)
+        # # layout.addWidget(courseLab, 1, 0)
+        # # layout.addWidget(courseNumLine, 1, 1)
+        # # layout.addWidget(areaLab, 2, 0)
+        # # layout.addWidget(areaLab, 2, 1)
+        # # layout.addWidget(titleLab, 3, 0)
+        # # layout.addWidget(titleLab, 3, 1)
+        # # layout.addWidget(list_box, 4, 0)
+        # # layout.addWidget(submit_but, 4, 1)
+
+        # frame = QFrame()
+        # frame.setLayout(outer_layout)
+
+        window = QMainWindow()
+        window.setWindowTitle('Princeton University Class Search')
+        window.setCentralWidget(centralFrame)
+        screenSize = QDesktopWidget().screenGeometry()
+        window.resize(screenSize.width()//2, screenSize.height()//2)
+
+        window.show()
+        exit(app.exec_())
 
         # when the user opens the application or has nothing in the input boxes, we need to display all classes in order of classid, department, coursenum, area, title. must have table format
 
@@ -88,20 +225,20 @@ def main(argv):
         # close the details box for a selected class item by hitting enter
 
         # user interface (prints table layout)
-        print("ClsId Dept CrsNum Area Title")
-        print("----- ---- ------ ---- -----")
-        wrapper = textwrap.TextWrapper(
-            width=72, break_long_words=False, subsequent_indent=(23 * ' '))
+        # print("ClsId Dept CrsNum Area Title")
+        # print("----- ---- ------ ---- -----")
+        # wrapper = textwrap.TextWrapper(
+        #     width=72, break_long_words=False, subsequent_indent=(23 * ' '))
 
-        # user interface: gets information from the database
-        # and prints to user
-        row = cursor.fetchone()
-        while row is not None:
-            unformatted_str = "{:>5} {:>4} {:>6} {:>4} {}".format(
-                str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
+        # # user interface: gets information from the database
+        # # and prints to user
+        # row = cursor.fetchone()
+        # while row is not None:
+        #     unformatted_str = "{:>5} {:>4} {:>6} {:>4} {}".format(
+        #         str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
 
-            print(wrapper.fill(unformatted_str))
-            row = cursor.fetchone()
+        #     print(wrapper.fill(unformatted_str))
+        #     row = cursor.fetchone()
 
 # exit(2) case handled by arg_parse module, exit(1) case handled on lines 11-18
 # If some other program has corrupted the reg.sqlite database file
