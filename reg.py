faba4d7dfeb9fd55cf61b3de4c4b7a02c76e6866
Sam Liang <darkdragn20000@gmail.com>
2021-03-07 21:02:32 -0500

    displaying classes right part 4

--- reg.py
+++ reg.py
@@ -112,7 +112,7 @@ def main(argv):
         for row in db_rows:
             line_string = "{:>6} {:>4} {:>6} {:>4} {}".format(
                 str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
-            list_box.addItem(row)
+            list_box.addItem(line_string)
 
         top_layout = QGridLayout()
         top_layout.setSpacing(0)
